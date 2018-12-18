423303f9986f2e296a3d045a9cc28e1a7d7612a2
kalbyimanie <craptalk8@gmail.com>
2018-12-18 23:40:09 +0700

    acdsacsadcsa

--- test.groovy
+++ test.groovy
@@ -2,31 +2,12 @@ pipeline{
 	agent{
 		node{
 			label "master"
+			priveleged true			
 		}
 	}
-	stages{
-		stage("Stage1"){
-			steps{
-				print "Test Stage1"
-			}
-		}
-		stage("Stage2"){
-			steps{
-				print "Test Stage2"
-			}
-		}
-		stage("Exec Shell"){
-			steps{
-				sh '''cd $WORKSPACE;chmod +x run.sh;./run.sh'''
-			}
-		}
-		stage("Test 3"){
-			steps{
-				// def var="Maruf"
-				// print "${var}"
-				print "Error here"
-			}
-
+	stage("Ping Host"){
+		steps{
+			sh ''' cd $WORKSPACE;chmod +x run.sh;./run.sh '''
 		}
 	}
 }
